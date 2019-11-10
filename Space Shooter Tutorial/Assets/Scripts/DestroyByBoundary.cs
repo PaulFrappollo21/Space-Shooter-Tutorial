b9999d62fe52012218b1cb7b087d001e13cca4ff
Paul Frappollo <55060905+PaulFrappollo21@users.noreply.github.com>
2019-11-10 01:21:21 -0500

    Basic Top Down Shooter

---
+++ Space Shooter Tutorial/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
